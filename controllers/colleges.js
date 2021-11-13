bb2f2e7022dd7da350d069a6d5d0f7ad72d40a6e
Apurva <apurvanagar@gmail.com>
2021-11-13 18:05:03 +0530

    colleges controller logic

--- controllers/colleges.js
+++ controllers/colleges.js
@@ -1,3 +1,40 @@
-export const getColleges = (req, res) => {
-  return res.json({ message: "/colleges Working" });
+import mongoose from "mongoose";
+import College from "../models/college.js";
+
+export const getColleges = async (req, res) => {
+  try {
+    const colleges = await College.find({});
+    res.status(200).json(colleges);
+  } catch (error) {
+    res.status(500).json({ errors: [error.message] });
+  }
+};
+
+export const getCollegeDetails = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ errors: ["Invalid College ID"] });
+  }
+
+  try {
+    const college = await College.findById(id);
+
+    if (!college)
+      return res.status(404).json({ errors: ["College Not Found"] });
+
+    const { state, country, studentCount, courses } = college;
+
+    const similarColleges = await College.find({
+      _id: { $ne: id },
+      state: state,
+      country: country,
+      studentCount: { $gte: studentCount - 100, $lte: studentCount + 100 },
+      courses: { $in: courses },
+    });
+
+    res.status(200).json({ college, similarColleges });
+  } catch (error) {
+    res.status(500).json({ errors: [error.message] });
+  }
 };
--- routes/colleges.js
+++ routes/colleges.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getColleges } from "../controllers/colleges.js";
+import { getColleges, getCollegeDetails } from "../controllers/colleges.js";
 
 const router = express.Router();
 
 router.get("/", getColleges);
+router.get("/:id", getCollegeDetails);
 
 export default router;
