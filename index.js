61b242f3a898b71bbd2723aefafdaaa12c8ae28e
Apurva <apurvanagar@gmail.com>
2021-11-16 02:49:31 +0530

    changed cors origin url to fe production

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://college-dashboard.vercel.app"],
     methods: ["GET"],
   })
 );
